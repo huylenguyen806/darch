e8e5530aa3aedf82817e75d69ccc32154d884532
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-03-22 22:33:01 +0700

    Update

--- resources/install.sh
+++ resources/install.sh
@@ -24,7 +24,6 @@ npm config set prefix '~/.npm-global'
 
 echo "Setting up pyenv"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 pyenv install 3.7.7
 pyenv install 2.7.17
 pyenv shell 2.7.17 && pip install neovim
